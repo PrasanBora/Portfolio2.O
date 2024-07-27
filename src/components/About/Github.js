5dc9621662145de78a0496007520bbed38420c41
Prasan Bora <prasanbora23@gmail.com>
2024-07-27 19:00:03 +0530

    commit

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -6,7 +6,7 @@ function Github() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
       <h1 className="project-heading" style={{ paddingBottom: "20px" }}>
-        Github  <strong className="purple">Contributions</strong>
+      Days I  <strong className="purple">Code</strong>
       </h1>
       <GitHubCalendar
         username="PrasanBora"
